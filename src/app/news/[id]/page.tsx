e83697ea8e779ba931786fe65f38c2c102785e51
ramaaloysius12 <ramaaloysius12@gmail.com>
2026-09-13 13:41:52 +0700

    Fix typo limport to import

--- src/app/news/[id]/page.tsx
+++ src/app/news/[id]/page.tsx
@@ -1,4 +1,4 @@
-limport Link from "next/link";
+import Link from "next/link";
 
 // 1. Tambahkan 'async' dan ubah tipe params menjadi 'Promise'
 export default async function PublicNewsDetailPage({ params }: { params: Promise<{ id: string }> }) {
